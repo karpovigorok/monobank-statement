b195a61fb61946a0da21e1a02e73ff34fdbf5e16
Igor Karpov <igor.karpov@kstt.ua>
2019-07-22 19:21:58 +0300

    migration added

---
+++ database/migrations/2019_07_17_153916_create_statements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid')->unique();
+            $table->longText('statement');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statements');
+    }
+}
